2a6b44370ea6a3a94475473f7fe4eedf03c1aaee
Twilight4 <electrolight071@gmail.com>
2023-04-02 16:19:25 +0200

    update

--- recon.sh
+++ recon.sh
@@ -51,19 +51,38 @@ scan_domain(){
   esac
 }
 
-echo "Generating recon report for $DOMAIN..."
+report_domain(){
+  DOMAIN=$1
+  DIRECTORY=${DOMAIN}_recon
+  echo "Generating recon report for $DOMAIN..."
 TODAY=$(date)
-echo "This scan was created on $TODAY" > $DIRECTORY/report
-if [ -f $DIRECTORY/nmap ];then
-  echo "Results for Nmap:" >> $DIRECTORY/report
-  grep -E "^\s*\S+\s+\S+\s+\S+\s*$" $DIRECTORY/nmap >> $DIRECTORY/report
+  echo "This scan was created on $TODAY" > $DIRECTORY/report
+  if [ -f $DIRECTORY/nmap ];then
+    echo "Results for Nmap:" >> $DIRECTORY/report
+      grep -E "^\s*\S+\s+\S+\s+\S+\s*$" $DIRECTORY/nmap >> $DIRECTORY/report
+  fi
+  if [ -f $DIRECTORY/dirsearch ];then
+    echo "Results for Dirsearch:" >> $DIRECTORY/report
+    cat $DIRECTORY/dirsearch >> $DIRECTORY/report
+  fi
+  if [ -f $DIRECTORY/crt ];then
+    echo "Results for crt.sh:" >> $DIRECTORY/report
+    jq -r ".[] | .name_value" $DIRECTORY/crt >> $DIRECTORY/report
+  fi
+}
+if [ $INTERACTIVE ];then
+  INPUT="BLANK"
+  while [ $INPUT != "quit" ];do
+    echo "Please enter a domain!"
+    read INPUT
+    if [ $INPUT != "quit" ];then
+      scan_domain $INPUT
+      report_domain $INPUT
+    fi
+  done
+else
+  for i in "${@:$OPTIND:$#}";do
+    scan_domain $i
+    report_domain $i
+  done
 fi
-if [ -f $DIRECTORY/dirsearch ];then
-  echo "Results for Dirsearch:" >> $DIRECTORY/report
-  cat $DIRECTORY/dirsearch >> $DIRECTORY/report
-fi
-if [ -f $DIRECTORY/crt ];then
-  echo "Results for crt.sh:" >> $DIRECTORY/report
-  jq -r ".[] | .name_value" $DIRECTORY/crt >> $DIRECTORY/report
-fi
-done 
