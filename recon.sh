73e4a9d555e1cc201eebef8060eaf9c062d8e460
Twilight4 <electrolight071@gmail.com>
2023-04-02 15:54:19 +0200

    update

--- recon.sh
+++ recon.sh
@@ -1,46 +1,61 @@
 #!/bin/bash
 PATH_TO_DIRSEARCH="/opt/tools/dirsearch"
-DOMAIN=$1
-DIRECTORY=${DOMAIN}_recon
-echo "Creating directory $DIRECTORY."
-mkdir $DIRECTORY
+
 nmap_scan()
 {
-nmap $DOMAIN > $DIRECTORY/nmap
-echo "The results of nmap scan are stored in $DIRECTORY/nmap."
+  nmap $DOMAIN > $DIRECTORY/nmap
+  echo "The results of nmap scan are stored in $DIRECTORY/nmap."
 }
 dirsearch_scan()
 {
-$PATH_TO_DIRSEARCH/dirsearch.py -u $DOMAIN -e php --simple-report=$DIRECTORY/dirsearch
-echo "The results of dirsearch scan are stored in $DIRECTORY/dirsearch."
+  $PATH_TO_DIRSEARCH/dirsearch.py -u $DOMAIN -e php --simple-report=$DIRECTORY/dirsearch
+  echo "The results of dirsearch scan are stored in $DIRECTORY/dirsearch."
 }
 crt_scan()
 {
-curl "https://crt.sh/?q=$DOMAIN&output=json" -o $DIRECTORY/crt
-echo "The results of cert parsing is stored in $DIRECTORY/crt."
+  curl "https://crt.sh/?q=$DOMAIN&output=json" -o $DIRECTORY/crt
+  echo "The results of cert parsing is stored in $DIRECTORY/crt."
 }
-case $2 in
-nmap-only)
-nmap_scan
-;;
-dirsearch-only)
-dirsearch_scan
-;;
-crt-only)
-crt_scan
-;;
-*)
-nmap_scan
-dirsearch_scan
-crt_scan
-;;
+getopts "m:" OPTION
+MODE=$OPTARG
+
+for i in "${@:$OPTIND:$#}" 1
+do
+
+  DOMAIN=$i
+  DIRECTORY=${DOMAIN}_recon
+  echo "Creating directory $DIRECTORY."
+  mkdir $DIRECTORY
+
+  case $MODE in
+  nmap-only)
+    nmap_scan
+    ;;
+  dirsearch-only)
+    dirsearch_scan
+    ;;
+  crt-only)
+    crt_scan
+    ;;
+  *)
+    nmap_scan
+    dirsearch_scan
+    crt_scan
+    ;;
 esac
-echo "Generating recon report from output files..."
+echo "Generating recon report for $DOMAIN..."
 TODAY=$(date)
-echo "This scan was created on $TODAY" > $DIRECTORY/report 
+echo "This scan was created on $TODAY" > $DIRECTORY/report
+if [ -f $DIRECTORY/nmap ];then 2
 echo "Results for Nmap:" >> $DIRECTORY/report
 grep -E "^\s*\S+\s+\S+\s+\S+\s*$" $DIRECTORY/nmap >> $DIRECTORY/report
+fi
+if [ -f $DIRECTORY/dirsearch ];then 3
 echo "Results for Dirsearch:" >> $DIRECTORY/report
-cat $DIRECTORY/dirsearch >> $DIRECTORY/report 
+cat $DIRECTORY/dirsearch >> $DIRECTORY/report
+fi
+if [ -f $DIRECTORY/crt ];then 4
 echo "Results for crt.sh:" >> $DIRECTORY/report
 jq -r ".[] | .name_value" $DIRECTORY/crt >> $DIRECTORY/report
+fi
+done 
