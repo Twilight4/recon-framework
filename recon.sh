b25a084cfd9cd1a6e87d26cc8f35b5870556aa16
Twilight4 <electrolight071@gmail.com>
2023-04-02 16:16:04 +0200

    update

--- recon.sh
+++ recon.sh
@@ -16,45 +16,53 @@ crt_scan()
   curl "https://crt.sh/?q=$DOMAIN&output=json" -o $DIRECTORY/crt
   echo "The results of cert parsing is stored in $DIRECTORY/crt."
 }
-getopts "m:" OPTION
-MODE=$OPTARG
 
-for i in "${@:$OPTIND:$#}" 1
-do
+while getopts "m:i" OPTION; do
+  case $OPTION in
+    m) 
+      MODE=$OPTARG
+      ;;
+    i)
+      INTERACTIVE=true
+      ;;
+  esac
+done
 
+scan_domain(){
   DOMAIN=$i
   DIRECTORY=${DOMAIN}_recon
   echo "Creating directory $DIRECTORY."
   mkdir $DIRECTORY
-
   case $MODE in
-  nmap-only)
-    nmap_scan
-    ;;
-  dirsearch-only)
-    dirsearch_scan
-    ;;
-  crt-only)
-    crt_scan
-    ;;
-  *)
-    nmap_scan
-    dirsearch_scan
-    crt_scan
-    ;;
-esac
+    nmap-only)
+      nmap_scan
+      ;;
+    dirsearch-only)
+      dirsearch_scan
+      ;;
+    crt-only)
+      crt_scan
+      ;;
+    *)
+      nmap_scan
+      dirsearch_scan
+      crt_scan
+      ;;
+  esac
+}
+
 echo "Generating recon report for $DOMAIN..."
 TODAY=$(date)
 echo "This scan was created on $TODAY" > $DIRECTORY/report
-  if [ -f $DIRECTORY/nmap ];then
+if [ -f $DIRECTORY/nmap ];then
   echo "Results for Nmap:" >> $DIRECTORY/report
   grep -E "^\s*\S+\s+\S+\s+\S+\s*$" $DIRECTORY/nmap >> $DIRECTORY/report
 fi
-  if [ -f $DIRECTORY/dirsearch ];then
+if [ -f $DIRECTORY/dirsearch ];then
   echo "Results for Dirsearch:" >> $DIRECTORY/report
   cat $DIRECTORY/dirsearch >> $DIRECTORY/report
 fi
-  if [ -f $DIRECTORY/crt ];then
+if [ -f $DIRECTORY/crt ];then
   echo "Results for crt.sh:" >> $DIRECTORY/report
   jq -r ".[] | .name_value" $DIRECTORY/crt >> $DIRECTORY/report
 fi
